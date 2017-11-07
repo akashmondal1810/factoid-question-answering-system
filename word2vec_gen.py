8348360223b3507e50c159ba75eccb86ff905264
GowthamRajG <graj5928@gmail.com>
2017-11-06 23:03:53 -0500

    [ #2 : WORD2VEC ] Implemented word2vec functionality

--- word2vec_gen.py
+++ word2vec_gen.py
@@ -1,3 +1,5 @@
+#code for converting word to vector
+#uses gensim library's word2vec implementations
 import nltk
 from gensim.models import Word2Vec
 from nltk.tokenize import word_tokenize
@@ -17,11 +19,12 @@ class wordvec(object):
  
 if __name__ == '__main__':
     print("word2vec done")
-    data_path = "C:\\Coursework\\CMSC726\\Project\\wsd_dev.txt"
+    data_path = "data\\sci_sample.csv"
 
     sentences = wordvec(data_path)
     model = Word2Vec(sentences)
-    print(model.most_similar('time', topn=5))
+    print(model.most_similar('A', topn=5))
+    # saving the model - optional
     # model.save("data/model/word2vec_gensim")
     # model.wv.save_word2vec_format("data/model/word2vec_org",
                                   # "data/model/vocabulary",
