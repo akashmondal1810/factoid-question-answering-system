95b2706537f77aecb577868057db70b71ae4bb7a
Karan Vivek Bhargava <karan.bhargava93@gmail.com>
2017-11-10 13:53:17 -0500

    [ #3 : WORD2VEC ] Milestone achieved - 48% accuracy on validation set
    Modified the input vector to be of the form [Q, ansA, ansB, ansC, ansD] and output to be the option e.g. 'A'. This made it a multiclass problem and it's giving decent accuracy. Moreover the class imbalance problem was also solved since we aren't duplicating any data.

--- logreg.py
+++ logreg.py
@@ -17,13 +17,27 @@ kC = 'answerC'
 kD = 'answerD'
 
 
-def formVector(data, lablelist):
-    x_train = [np.hstack((model.sv(x[kTEXT_FIELD]), model.sv(x["answer" + i])))
-               for i in lablelist for x in data]
-    # print("new total x_train length", x_train.shape[0])
-    y_train = array(list(1 if x["answer" + x[kTARGET_FIELD]] == x["answer" + i] else 0
-                         for i in lablelist for x in data))
-    # print("new total y_train length", y_train.shape[0])
+def formVector(data):
+    # lablelist = ["A", "B", "C", "D"]w
+    x_train = []
+    y_train = []
+    for x in data:
+        # form the large array
+        v = np.hstack((model.sv(x[kTEXT_FIELD]), model.sv(x[kA])))
+        v = np.hstack((v, model.sv(x[kB])))
+        v = np.hstack((v, model.sv(x[kC])))
+        v = np.hstack((v, model.sv(x[kD])))
+
+        y = x[kTARGET_FIELD]
+
+        x_train.append(v)
+        y_train.append(y)
+
+    # x_train = [
+    #     for i in lablelist for x in data]
+    # # print("new total x_train length", x_train.shape[0])
+    # y_train = array(list(1 if x["answer" + x[kTARGET_FIELD]] == x["answer" + i] else 0
+    #                      for i in lablelist for x in data))
     return x_train, y_train
 
 
@@ -43,27 +57,25 @@ if __name__ == "__main__":
     # print("Length of train: ", len(train), " test: ", len(test))
     # print("% train vs test:", len(train)/(len(train) + len(test)))
 
-    labels = []
-    for line in train:
-        if not line[kTARGET_FIELD] in labels:
-            labels.append(line[kTARGET_FIELD])
+    # labels = []
+    # for line in train:
+    #     if not line[kTARGET_FIELD] in labels:
+    #         labels.append(line[kTARGET_FIELD])
 
-    # print(len(labels))
+    # labels = sorted(labels)
 
     # test_labels = []
     # for line in test:
     #     if not line[kTARGET_FIELD] in test_labels:
     #         test_labels.append(line[kTARGET_FIELD])
 
-    lablelist = ["A", "B", "C", "D"]
-
     model = w2v()
 
     # Assigining 1 to correct answer and 0 to wrong answer
-    x_train, y_train = formVector(train, lablelist)
+    x_train, y_train = formVector(train)
     # print("new total x_train length", x_train.shape[0])
 
-    x_test, y_test = formVector(test, lablelist)
+    x_test, y_test = formVector(test)
     # print("new total x_test length", x_test.shape[0])
 
     # print(y_train)
@@ -83,8 +95,11 @@ if __name__ == "__main__":
     print(accuracy_val)
 
     predictions = lr.predict(x_test)
+
+    # print(predictions[:10])
+
     o = DictWriter(open("testpredictions.csv", 'w'), ["id", "correctAnswer"])
     o.writeheader()
     for ii, pp in zip([x['id'] for x in test], predictions):
-        d = {'id': ii, 'correctAnswer': labels[pp]}
+        d = {'id': ii, 'correctAnswer': pp}
         o.writerow(d)
