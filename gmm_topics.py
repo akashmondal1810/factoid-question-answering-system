bf1f50d51371bad839814a4d05be7a0760dda24c
grahamski2323 <ganto@math.umd.edu>
2017-12-05 18:31:14 -0500

    Updating Topic Modeling, Initial GMM clustering of words

---
+++ gmm_topics.py
@@ -0,0 +1,81 @@
+# This is for Gaussian Mixture Model, to show probability within each clsuter
+import os
+import numpy as np
+from gensim.models import Word2Vec
+from word2vec_gen import *
+import sklearn
+from sklearn.mixture import GaussianMixture, BayesianGaussianMixture
+from csv import DictReader, DictWriter
+
+# inputs: word2vec from wiki corpus data
+# outputs: file of word, ordered clsuter, probs, centroids, varaince
+# in one csv file
+
+def word_clusters(out_file,vocab,probs,means,covs):
+    # create output dictionary of words, order cluster idx, probs, centroids, (not saving varaince atm)
+    word_clus = [dict() for x in range(len(vocab))]
+    
+    for ii in range(len(vocab)):
+        word = vocab[ii]
+        clus_prob = probs[ii]
+        top_id = list()
+        for idx in reversed(np.argsort(clus_prob)):
+            top_id.append(idx)
+        word_clus[ii] = {'word':word,'clusterorder':top_id,'clusprob':clus_prob[top_id],'clusmeans':means[top_id]};
+        # excluding variances now - is it needed?
+    
+    keys = word_clus[0].keys()
+ #  print(keys)
+    with open(out_file,'w',newline='',encoding='utf-8') as output: # trying not wb
+        f = DictWriter(output,keys)
+        f.writeheader()
+        f.writerows(word_clus)
+
+## MAIN
+if __name__ == "__main__":
+# read in word2vec stuff
+    print("Training word2vec...")
+
+    filepath = "data/word2vec.dat"
+    if os.path.exists(filepath):
+        model = w2v(load_data=True, old_data=filepath)
+        print("Existing data loaded.")
+    else:
+        model = w2v(data_path='data/wiki_corpus.txt')
+        model.model.save(filepath)
+        print("New word2vec data created.")
+
+# read in vocab from word2vec set
+    vocab = model.vocab #()   
+    #print(vocab) # so voacb is all word of w2v model
+    train = [model.wv(x) for x in vocab]
+    #print(train[0])
+   # print(len(vocab))
+   # so vocab and train of same length, train is w2v represnetations
+
+
+# from this, run on vectors for GMM
+# LOOK UP GMM STUFF ON THEIR HELP SITE
+    gmm = GaussianMixture(n_components=20) # all other options should be defaults
+    gmm.fit(train)
+    weights = gmm.weights_ # these are weights of GMM
+#    print(len(weights))
+    means = gmm.means_ # these are means of GMM clusters
+#    print(len(means))
+    probs = gmm.predict_proba(train)
+    covs = gmm.covariances_
+    print("done first step")
+
+# write to a csv file
+    out_file = 'gmm_results.csv'
+    word_clusters(out_file,vocab,probs,means,covs)
+    
+    gmm = BayesianGaussianMixture(n_components=20,max_iter=200) # they could potentially zero out some compoennts 
+    gmm.fit(train)
+    means = gmm.means_
+    probs = gmm.predict_proba(train)
+    covs = gmm.covariances_
+    out_file = 'gmm_results_var30.csv'
+    word_clusters(out_file,vocab,probs,means,covs)
+
+
